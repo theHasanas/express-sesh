444e0acd514f5cd57a61c6bee9e34177d842aeda
theHasanas <30086691+theHasanas@users.noreply.github.com>
2021-04-04 18:52:59 +0300

    Add ip package

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
-const { networkInterfaces } = require("os");
+const ip = require("ip");
 const db = require("./db/models");
 
 // importing routers
@@ -28,26 +28,11 @@ app.use("/users", usersRouter);
 // start server
 const runApp = async () => {
   try {
-    const nets = networkInterfaces();
-    const results = Object.create(null);
-
-    for (const name of Object.keys(nets)) {
-      for (const net of nets[name]) {
-        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-        if (net.family === "IPv4" && !net.internal) {
-          if (!results[name]) {
-            results[name] = [];
-          }
-          results[name].push(net.address);
-        }
-      }
-    }
-
     await db.sequelize.sync({ alter: true });
 
     app.listen(8000, () => {
       console.log("Express app started succeffully");
-      console.log(`Running on ${results["en0"][0]}:8000`);
+      console.log(`Running on ${ip.address()}:8000`);
     });
   } catch (error) {
     console.log("Failed to start server:", error);
